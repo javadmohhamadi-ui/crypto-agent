8492d5cb889ed142be009b28734e7d28b859667e
javadmohhamadi-ui <javadmohhamadi@gmail.com>
2026-06-27 16:49:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-import os
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = "8290825217:AAHfZHzGzxRgXZEJfYgdTifVl7FdacW6X-A"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ربات روشنه علیرضا 👑")
+    await update.message.reply_text("ایجنت رمز ارز آنلاین شد!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    await update.message.reply_text(f"پیامت رسید: {text}")
+    await update.message.reply_text(update.message.text)
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-app.add_handler(MessageHandler(filters.TEXT, echo))
-
-app.run_polling()
-
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
