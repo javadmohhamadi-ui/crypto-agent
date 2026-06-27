7fa9ff102bbc964cb76b0c4432b9e0512aad846f
javadmohhamadi-ui <javadmohhamadi@gmail.com>
2026-06-27 16:44:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app = ApplicationBuilder().token(TOKEN).build()
 
-    app.run_polling()
+app.add_handler(MessageHandler(filters.TEXT, echo))
+
+app.run_polling()
+
 
 if __name__ == "__main__":
     main()
